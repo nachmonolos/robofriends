93ea091f5903e37e6be426f8fe5a0da6cab840f1
nachmonolos <68893120+nachmonolos@users.noreply.github.com>
2020-11-12 19:01:26 +0800

    Added CardList

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
-  <Hello />,
+  <CardList robots={robots} />
+  ,
   document.getElementById('root')
 );
 
