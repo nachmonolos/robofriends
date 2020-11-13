87ee55c44a35b01a57ac0e406d6e6bf76058cbd1
nachmonolos <68893120+nachmonolos@users.noreply.github.com>
2020-11-13 18:39:19 +0800

    Capitalize Search Box placeholer

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-      <input onChange={searchChange} className='pa3 ba b--green bg-lightest-green' type='search' placeholder='search robots' />
+      <input onChange={searchChange} className='pa3 ba b--green bg-lightest-green' type='search' placeholder='Search Robots' />
     </div>
 
   );
